49aa13254e88bf7352ef91d0bc8918778859fd61
souravkunduss1@outlook.com <souravkunduss1@outlook.com>
2022-05-11 12:56:05 +0600

    180144

---
+++ Controllers/Admin/CLOController.cs
@@ -0,0 +1,36 @@
+﻿using SyllabusMaker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SyllabusMaker.Controllers.Admin
+{
+    public class CLOController : Controller
+    {
+        SyllabusMakerEntities db = new SyllabusMakerEntities();
+        // GET: CLO
+        public ActionResult Index()
+        {
+            var q = db.CLOes.ToList();
+            return View(q);
+        }
+
+        public ActionResult Create()
+        {
+            List<Course> CourseList = db.Courses.ToList();
+            ViewBag.CourseList = new SelectList(CourseList, "CourseId", "CourseCode");
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(CLO clo)
+        {
+            //List<Course> CourseList = db.Courses.ToList();
+            //ViewBag.CourseList = new SelectList(CourseList, "CourseId", "CourseCode");
+            db.CLOes.Add(clo);
+            db.SaveChanges();
+            return RedirectToAction("Index", "CLO");
+        }
+    }
+}
---
+++ Controllers/Admin/CourseObjectivesController.cs
@@ -0,0 +1,36 @@
+﻿using SyllabusMaker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SyllabusMaker.Controllers.Admin
+{
+    public class CourseObjectivesController : Controller
+    {
+        SyllabusMakerEntities db = new SyllabusMakerEntities();
+        // GET: CourseObjectives
+        public ActionResult Index()
+        {
+            var q = db.CourseObjectives.ToList();
+            return View(q);
+        }
+
+        public ActionResult Create()
+        {
+            List<Course> CourseList = db.Courses.ToList();
+            ViewBag.CourseList = new SelectList(CourseList, "CourseId", "CourseCode");
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(CourseObjective co)
+        {
+            //List<Course> CourseList = db.Courses.ToList();
+            //ViewBag.CourseList = new SelectList(CourseList, "CourseId", "CourseCode");
+            db.CourseObjectives.Add(co);
+            db.SaveChanges();
+            return RedirectToAction("Index", "CourseObjectives");
+        }
+    }
+}
