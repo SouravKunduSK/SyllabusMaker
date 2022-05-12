418745910793ecdd66dcb92545509b59eb85032a
Soumay Kanti Das <soumaykanti2859@gmail.com>
2022-05-12 10:40:00 +0600

    180148

---
+++ Controllers/Admin/AssessmentStrategiesController.cs
@@ -0,0 +1,33 @@
+﻿using SyllabusMaker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SyllabusMaker.Controllers.Admin
+{
+    public class AssessmentStrategiesController : Controller
+    {
+        SyllabusMakerEntities db = new SyllabusMakerEntities();
+        // GET: AssessmentStrategies
+        public ActionResult Index()
+        {
+            var q = db.AssessmentStrategies.ToList();
+            return View(q);
+        }
+        public ActionResult Create()
+        {
+            return View();  
+        }
+
+        [HttpPost]
+        public ActionResult Create(AssessmentStrategie a)
+        {
+            db.AssessmentStrategies.Add(a);
+            db.SaveChanges();
+            return RedirectToAction("Index", "AssessmentStrategies");
+        }
+
+    }
+}
