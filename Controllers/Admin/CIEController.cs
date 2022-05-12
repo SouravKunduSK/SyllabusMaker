8ec8b07e80bcc367607df452f96f0f3fb88a9bbd
Soumay Kanti Das <soumaykanti2859@gmail.com>
2022-05-12 15:41:07 +0600

    180148

---
+++ Controllers/Admin/CIEController.cs
@@ -0,0 +1,20 @@
+﻿using SyllabusMaker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SyllabusMaker.Controllers.Admin
+{
+    public class CIEController : Controller
+    {
+        SyllabusMakerEntities db = new SyllabusMakerEntities();
+        // GET: CIE
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+    }
+}
