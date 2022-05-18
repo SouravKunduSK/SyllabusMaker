7dd1d67d9859c356fd5597599329d5add8b51c60
Soumay Kanti Das <soumaykanti2859@gmail.com>
2022-05-18 11:34:22 +0600

    180148

---
+++ Controllers/Admin/ResourceController.cs
@@ -0,0 +1,38 @@
+﻿using SyllabusMaker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SyllabusMaker.Controllers.Admin
+{
+    public class ResourceController : Controller
+    {
+        SyllabusMakerEntities db = new SyllabusMakerEntities();
+        // GET: Resource
+        public ActionResult Index()
+        {
+            var q = db.Books.ToList();
+            return View(q);
+        }
+        public ActionResult Create()
+        {
+            List<BookType> TypeList = db.BookTypes.ToList();
+            ViewBag.TypeList = new SelectList(TypeList, "BooktypeId", "Type");
+            return View();
+        }
+        [HttpPost]
+
+        public ActionResult Create(Book b)
+        {
+            List<BookType> TypeList = db.BookTypes.ToList();
+            ViewBag.TypeList = new SelectList(TypeList, "BookTypeId", "Type");
+            db.Books.Add(b);
+            db.SaveChanges();
+            return RedirectToAction("Index","Resources");
+        }
+    
+
+    }
+}
