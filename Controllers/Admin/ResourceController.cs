f3eebbf6c4cb9caad8cd4470b348dfa9025e39bd
Soumay Kanti Das <soumaykanti2859@gmail.com>
2022-05-19 00:41:58 +0600

    180148

--- Controllers/Admin/ResourceController.cs
+++ Controllers/Admin/ResourceController.cs
@@ -19,18 +19,21 @@ namespace SyllabusMaker.Controllers.Admin
         public ActionResult Create()
         {
             List<BookType> TypeList = db.BookTypes.ToList();
+            List<Course> CourseList = db.Courses.ToList();
             ViewBag.TypeList = new SelectList(TypeList, "BooktypeId", "Type");
+            ViewBag.CourseList = new SelectList(CourseList, "CourseId", "CourseCode");
+
             return View();
         }
         [HttpPost]
 
         public ActionResult Create(Book b)
         {
-            List<BookType> TypeList = db.BookTypes.ToList();
-            ViewBag.TypeList = new SelectList(TypeList, "BookTypeId", "Type");
+            //List<BookType> TypeList = db.BookTypes.ToList();
+            //ViewBag.TypeList = new SelectList(TypeList, "BookTypeId", "Type");
             db.Books.Add(b);
             db.SaveChanges();
-            return RedirectToAction("Index","Resources");
+            return RedirectToAction("Index","Resource");
         }
     
 
