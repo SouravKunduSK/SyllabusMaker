9d1550cf1240f0f48262580ebc4cb3001bd41560
Soumay Kanti Das <soumaykanti2859@gmail.com>
2022-05-22 12:12:15 +0600

    180148

---
+++ Controllers/Admin/TeachingStrategieController.cs
@@ -0,0 +1,33 @@
+﻿using SyllabusMaker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SyllabusMaker.Controllers.Admin
+{
+    public class TeachingStrategieController : Controller
+    {
+        // GET: TeachingStrategie
+        SyllabusMakerEntities db = new SyllabusMakerEntities();
+        public ActionResult Index()
+        {
+            var q = db.TeachingStrategies.ToList();
+            return View(q);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(TeachingStrategie t)
+        {
+            db.TeachingStrategies.Add(t);
+            db.SaveChanges();
+            return RedirectToAction ("Index","TeachingStrategie");
+        }
+
+    }
+
+}
